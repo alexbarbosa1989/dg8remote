6b622a3efcbffb58762e73983f251a2669a9f8ab
Alexander Barbosa Ayala <alexanderbarbosaayala@gmail.com>
2022-01-20 09:18:27 -0500

    Update RemoteQueryInitializer.java

--- src/main/java/com/redhat/dg8remote/RemoteQueryInitializer.java
+++ src/main/java/com/redhat/dg8remote/RemoteQueryInitializer.java
@@ -3,6 +3,8 @@ package com.redhat.dg8remote;
 import org.infinispan.protostream.SerializationContextInitializer;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 
+// book.proto file is generated in the compilation automatically
+// book_sample must be defined as indexed-entity in the RHDG server cache "books"
 @AutoProtoSchemaBuilder(
 	      includeClasses = {
 	            Book.class
