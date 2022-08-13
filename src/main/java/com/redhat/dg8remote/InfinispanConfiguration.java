5aede67d090416db0840cb9acc81eef26857e6f9
alexbarbosa1989 <alexanderbarbosaayala@gmail.com>
2022-08-13 17:36:10 -0400

    add protobuf marshaller

---
+++ src/main/java/com/redhat/dg8remote/InfinispanConfiguration.java
@@ -0,0 +1,23 @@
+package com.redhat.dg8remote;
+
+import org.infinispan.commons.marshall.ProtoStreamMarshaller;
+import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import com.redhat.dg8remote.controller.RemoteQueryInitializerImpl;
+
+@Configuration
+public class InfinispanConfiguration {
+
+   @Bean
+   @Order(Ordered.HIGHEST_PRECEDENCE)
+   public InfinispanRemoteCacheCustomizer caches() {
+      return b -> {
+         // Add marshaller in the client, the class is generated from the interface in compile time
+         b.addContextInitializer(new RemoteQueryInitializerImpl());
+      };
+   }
+}
