6d8bce9d29b16df0792b9264ce3cfdb591f15452
Alexander Barbosa Ayala <alexanderbarbosaayala@gmail.com>
2022-09-11 16:35:07 -0500

    fix book definition

--- src/main/java/com/redhat/dg8remote/controller/Book.java
+++ src/main/java/com/redhat/dg8remote/controller/Book.java
@@ -27,28 +27,4 @@ public class Book {
 		this.publicationYear = publicationYear;
 	}
 
-	@Override
-   	public String toString() {
-      	return "Book{" +
-            "title='" + title + '\'' +
-            ", description='" + description + '\'' +
-            ", publicationYear='" + publicationYear + '\'' +
-            '}';
-   	}
-
-	@Override
-	public int hashCode() {
-		  return Objects.hash(title, description,publicationYear);
-	}
-
-	@Override
-   	public boolean equals(Object o) {
-      	if (this == o) return true;
-      	if (o == null || getClass() != o.getClass()) return false;
-      	Book that = (Book) o;
-      	return Objects.equals(title, that.title) &&
-        	     Objects.equals(description,that.description) &&
-		     Objects.equals(publicationYear, that.publicationYear);
-   }
-
 }
