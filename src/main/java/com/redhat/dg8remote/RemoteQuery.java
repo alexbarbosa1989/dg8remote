51157fa071cd8a3ed0c708c248542ee0c775380a
Alexander Barbosa Ayala <alexanderbarbosaayala@gmail.com>
2023-02-25 17:16:37 -0500

    upgrade to RHDG 8.4 and Protostream 4.5

--- src/main/java/com/redhat/dg8remote/RemoteQuery.java
+++ src/main/java/com/redhat/dg8remote/RemoteQuery.java
@@ -1,17 +1,17 @@
 package com.redhat.dg8remote;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.Search;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.query.dsl.Query;
 import org.infinispan.query.dsl.QueryFactory;
-import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+
+import static org.infinispan.query.remote.client.ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME;
+
 
 public class RemoteQuery {
 	public static void main(String[] args) throws Exception {
@@ -23,25 +23,24 @@ public class RemoteQuery {
 
 	      RemoteCacheManager remoteCacheManager = new RemoteCacheManager(clientBuilder.build());
 	      
-	      // Grab the generated protobuf schema and registers in the server.
-	      Path proto = Paths.get(RemoteQuery.class.getClassLoader()
-	            .getResource("proto/book.proto").toURI());
-	      String protoBufCacheName = ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME;
-	      remoteCacheManager.getCache(protoBufCacheName).put("book.proto", Files.readString(proto));
+		   // Create and add the Protobuf schema in the server
+		   addBookSchema(remoteCacheManager);
 
 	      // Obtain the 'books' remote cache
-	      RemoteCache<Object, Object> remoteCache = remoteCacheManager.getCache("books");
+	      RemoteCache<Long, Book> remoteCache = remoteCacheManager.getCache("books");
 
 	      // Add some Books
 	      Book book1 = new Book("Infinispan in Action", "Learn Infinispan with using it", 2015);
 	      Book book2 = new Book("Cloud-Native Applications with Java and Quarkus", "Build robust and reliable cloud applications", 2019);
 
-	      remoteCache.put(1, book1);
-	      remoteCache.put(2, book2);
+	      remoteCache.put(1L, book1);
+	      remoteCache.put(2L, book2);
 
 	      // Execute a full-text query
 	      QueryFactory queryFactory = Search.getQueryFactory(remoteCache);
-	      Query<Book> query = queryFactory.create("FROM book_sample.Book WHERE title:'java'");
+	      Query<Book> query = queryFactory.create("FROM book_sample.Book WHERE title LIKE :contains");
+
+		  query.setParameter("contains","%java%");
 
 	      List<Book> list = query.execute().list(); // Voila! We have our book back from the cache!
 	     
@@ -53,4 +52,15 @@ public class RemoteQuery {
 	      }
 	   }
 
+	   private static void addBookSchema(RemoteCacheManager cacheManager) {
+		// Retrieve metadata cache
+		RemoteCache<String, String> metadataCache =
+			  cacheManager.getCache(PROTOBUF_METADATA_CACHE_NAME);
+  
+		// Define the new schema on the server too
+		GeneratedSchema schema = new RemoteQueryInitializerImpl();
+		metadataCache.put(schema.getProtoFileName(), schema.getProtoFile());
+	 }
+
 }
+
